556f3b462d15eeea5a8be0edb1bf2fe8e61b1322
chowkingman <hkkevinchow@gmail.com>
2024-04-15 20:17:07 +0000

    update prompt and UI

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# fakeETData
-Just some fake mocked data
+# Talk to Enterprise metadata
+A chatbot that can chat with you about Enterprise metadata
+
+# Steps to run this program
+1. create a venv by running this under Git Bash
+'c:/Program Files/devtools/Python312/python' -m venv $HOME/Documents/myenv
+2. run the venv
+source $HOME/Documents/myenv/Scripts/activate
+3. install dependencies, nagivate to the repo and run this
+pip install -r requirements.txt
+4. run the standalone Chatbot GUI
+streamlit run main.py
+4. use chrome, put URL localhost:8501
--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 import streamlit as st
 import pandas as pd
 import matplotlib as plt
+import os
 from pandasai import SmartDataframe
 from pandasai.llm.openai import OpenAI
 from pandasai import Agent
 
-#ingest mock data to dataframe
-# sdf1 = SmartDataframe('MOCK_APP_DATA.csv', config={"llm": llm})
-# sdf2 = SmartDataframe('MOCK_RESOURCE_DATA.csv', config={"llm": llm})
-
-# agent = Agent(
-#     [sdf1, sdf2],
-#     config={"llm": llm},
-#     description=""
-# )
-
-# response = agent.chat("make graph to show the online status in each app_org")
-# print(response)
-
-# GUI
-st.title("Talk to Enterprise Meta Data")
-st.write("Hi, I am a chatbot that can understand human language and provide insights of Enterprise meta data!")
+# GUI and chatbot connection
+st.title("PIT BOT")
+st.write("Hi, I am PIT Bot, I collected Enterprise meta data and I can understand human language and happy to provide you insights about the data!")
 st.session_state.prompt_history = []
 
 with st.form("Question"):
@@ -29,17 +17,16 @@ with st.form("Question"):
     if submitted:
         with st.spinner():
             llm = OpenAI()
-
-            sdf1 = SmartDataframe('MOCK_APP_DATA.csv', config={"llm": llm})
-            sdf2 = SmartDataframe('MOCK_RESOURCE_DATA.csv', config={"llm": llm})
+            sdf1 = SmartDataframe('mock_app_data.csv', config={"llm": llm})
+            sdf2 = SmartDataframe('mock_resource_data.csv', config={"llm": llm})
 
             agent = Agent(
                 [sdf1, sdf2],
                 config={"llm": llm},
                 description=
-                "You have two tables, the MOCK_APP_DATA has the basic descriptive information of all the applications, " +
+                "You have two tables, the mock_app_data has the basic descriptive information of all the applications, " +
                 "and each application has an ID in the column id which is an unique identifier of each application. " + 
-                "The MOCK_RESOURCE_DATA table indicated the resources each application use. Each application can have multiple resources, and each resource has an app_id indicated which application the resource(s) belongs to." +
+                "The mock_resource_data table indicated the resources each application use. Each application can have multiple resources, and each resource has an app_id indicated which application the resource(s) belongs to." +
                 "Using the two tables, you can identify the descriptive information of each application, such as the owner of the application, the email address of the owner, " + 
                 "the organization the app belongs to, and the resources being used by each app and the online status of the application. " +
                 "Your job is to provide insights to the non-techincal users about the application information."
@@ -49,6 +36,7 @@ with st.form("Question"):
 
             if response is not None:
                 st.write(response)
-
             st.session_state.prompt_history.append(question)
 
+if st.button("Clear the history"):
+    st.session_state.prompt_history = []
